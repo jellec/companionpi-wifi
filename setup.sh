e86609a56402183be4ccfcd394af399f5c849e5b
Jelle Coremans <jellecoremans@gmail.com>
2025-05-24 12:29:10 +0200

    edit

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "📥 Downloading CompanionPi-wifi installer..."
 
-# Download ZIP van repo
-wget -q https://github.com/jellec/companionpi-wifi/archive/refs/heads/main.zip -O companionpiwifi.zip
+# Download zip met correcte bestandsnaam
+wget -q https://github.com/jellec/companionpi-wifi/archive/refs/heads/main.zip -O companionpi-wifi.zip
 
-# Uitpakken
-unzip -q companionpiwifi.zip
+# Unzip en opruimen
+unzip -q companionpi-wifi.zip
+rm companionpi-wifi.zip
+
+# Ga naar map en voer installatie uit
 cd companionpi-wifi-main
-rm companionpiwifi.zip
-
-# Installatie starten
 chmod +x install.sh
 ./install.sh
